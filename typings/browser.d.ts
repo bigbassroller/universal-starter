a2b408c3b5932b9d43b3b2661ac7e5b14aaf6e93
Michael Chavez <bigbassroller@gmail.com>
2016-02-25 08:37:35 -0800

    added typings folder

---
+++ typings/browser.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/express/express.d.ts" />
+/// <reference path="browser/ambient/mime/mime.d.ts" />
+/// <reference path="browser/ambient/ng2/ng2.d.ts" />
+/// <reference path="browser/ambient/node/node.d.ts" />
+/// <reference path="browser/ambient/serve-static/serve-static.d.ts" />
+/// <reference path="browser/ambient/zone.js/zone.js.d.ts" />
